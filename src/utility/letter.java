4365be5bf3979e2fe4eaad273125e9082fead625
shdShadow <simomosino@gmail.com>
2023-11-13 19:58:13 +0100

    Base class for a letter

---
+++ src/utility/letter.java
@@ -0,0 +1,22 @@
+package utility;
+public class letter{
+    private char character;
+    private int value;
+    //Player fields will be set when the card will be randomly drawn
+    private player player;
+
+    //GETTERS AND SETTERS
+    
+    public char getCharacter() {
+    return character;
+}
+
+    public player getPlayer() {
+    return player;
+}
+
+    public int getValue() {
+    return value;
+}
+
+}
