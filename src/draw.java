b3e69e7fe793f0d5e4ee330062bc168c7e3dea08
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-15 00:15:52 +0100

    Update draw.java

--- src/draw.java
+++ src/draw.java
@@ -3,22 +3,23 @@ public class draw {
 
     public draw(int righe, int colonne) {
         matriceDiGioco = new char[righe][colonne];
-        
-        initializeMatrix();//Inizializzo la matrice con dei valori di esempio
+
+        initializeMatrix();// Inizializzo la matrice con dei valori di esempio
     }
 
     private void initializeMatrix() {
-        for (int i = 0; i < matriceDiGioco.length; i++) {        //scorro le righe
-            for (int j = 0; j < matriceDiGioco[i].length; j++) {  //scorro le colonne
-               
-                matriceDiGioco[i][j] = (char) ('A' + (int) (Math.random() * 26));  //genero lettere casuali dalla A alla Z coi valori ASCII
+        for (int i = 0; i < matriceDiGioco.length; i++) { // scorro le righe
+            for (int j = 0; j < matriceDiGioco[i].length; j++) { // scorro le colonne
+
+                matriceDiGioco[i][j] = (char) ('A' + (int) (Math.random() * 26)); // genero lettere casuali dalla A alla
+                                                                                  // Z coi valori ASCII
             }
         }
     }
 
     public void mostraMatrice() {
-       
-        for (int i = 0; i < matriceDiGioco.length; i++) {  //stampo la griglia
+
+        for (int i = 0; i < matriceDiGioco.length; i++) { // stampo la griglia
             for (int j = 0; j < matriceDiGioco[i].length; j++) {
                 System.out.print(matriceDiGioco[i][j] + " ");
             }
@@ -28,13 +29,12 @@ public class draw {
 
     // Aggiorna la posizione nella matrice con un valore specifico
     public void updateMatrix(int righe, int colonne, char value) {
-        if (righe >= 0 && righe < matriceDiGioco.length && colonne >= 0 && colonne < matriceDiGioco[0].length) { //controllo se la lettera inserita è compresa nella matrice
-            matriceDiGioco[righe][colonne] = value;   //se è compresa
+        if (righe >= 0 && righe < matriceDiGioco.length && colonne >= 0 && colonne < matriceDiGioco[0].length) { // controllo se la lettera inserita è compresa nella matrice
+                                                                                                                 
+            matriceDiGioco[righe][colonne] = value; // se è compresa
         } else {
-            System.out.println("Posizione non valida");    //se non è compresa
+            System.out.println("Posizione non valida"); // se non è compresa
         }
     }
 
-  
 }
-
