d52db9541d7e084dcfdd51aef3b5ef4c8c356e1a
shdShadow <simomosino@gmail.com>
2023-11-14 17:52:28 +0100

    Test main

--- src/app.java
+++ src/app.java
@@ -1,5 +1,17 @@
-public class app{
-    public static void main(String[] args) {
-        System.out.println("suca");
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import utility.*;
+import xml.*;
+
+public class app {
+  public static void main(String[] args)
+      throws SAXException, IOException, ParserConfigurationException {
+    ArrayList<letter> tmp = new ArrayList<letter>();
+    tmp = xml.fileManager.getLetterValues();
+    for (letter letter : tmp) {
+        System.out.println(letter.toString());
     }
+  }
 }
