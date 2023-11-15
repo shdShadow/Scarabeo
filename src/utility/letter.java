a4872151616a9f2c8c2459621b2e961f517d3a0b
shdShadow <simomosino@gmail.com>
2023-11-15 08:37:24 +0100

    Aggiunti costruttori

--- src/utility/letter.java
+++ src/utility/letter.java
@@ -1,22 +1,45 @@
 package utility;
-public class letter{
-    private char character;
-    private int value;
-    //Player fields will be set when the card will be randomly drawn
-    private player player;
 
-    //GETTERS AND SETTERS
-    
-    public char getCharacter() {
-    return character;
-}
+public class letter {
+  private char character;
+  private Integer value;
+  // Player fields will be set when the card will be randomly drawn
+  private player player;
 
-    public player getPlayer() {
-    return player;
-}
+  // GETTERS AND SETTERS
 
-    public int getValue() {
-    return value;
-}
+  public char getCharacter() { return character; }
 
+  public player getPlayer() { return player; }
+
+  public Integer getValue() { return value; }
+
+  //   CONSTRUCTORS
+
+  public letter() {
+    character = ' ';
+    value = 0;
+    player = new player();
+  }
+
+  public letter(char _character, Integer _value, player _player) {
+    character = _character;
+    value = _value;
+    player = _player;
+  }
+
+  public letter(char _character, Integer _value) {
+    character = _character;
+    value = _value;
+    player = new player();
+  }
+
+  // METHODS
+
+  public String toString() {
+    String s = "";
+    s += "Char: " + character + " Value: " + value +
+         " Player: " + player.getPlayer_name();
+    return s;
+  }
 }
