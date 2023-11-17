c25b7f5728984251e32de8cd7a1b36702870e00d
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-17 15:22:56 +0100

    Gestione turni giocatori e modifiche
    Modificato il vettore di lettere da char a letter(classe) mantenendola 17x17 e creato il metodo mostraGriglia che gestisce il turno dei due player colorando la lettera di rosso se appartiene al giocatore 1, mentre di blu se appartiene al giocatore 2

--- src/utility/matriceScarabeo.java
+++ src/utility/matriceScarabeo.java
@@ -6,12 +6,12 @@ import java.util.Random;
 
 public class matriceScarabeo extends JFrame {
     private JLabel[][] grigliaDiGioco;    //griglia di gioco
-    private char[][] lettereGriglia;     //valori all'interno delle celle(lettere)
+    private letter[][] lettereGriglia;     //valori all'interno delle celle(lettere)
     private boolean turnoGiocatore1; // Flag per alternare i giocatori
 
     public matriceScarabeo() {
         grigliaDiGioco = new JLabel[17][17];
-        lettereGriglia = new char[17][17];
+        lettereGriglia = new letter[17][17];
         turnoGiocatore1 = true; // Inizia il primo giocatore
 
         setTitle("Gioco Scarabeo");
@@ -31,7 +31,29 @@ public class matriceScarabeo extends JFrame {
         setVisible(true);
     }
 
-    
+    public void aggiungiLettera(int riga, int colonna, char lettera, int valore,player player, punto p) {//metodo per aggiungere lettere nella posizione specificata che viene passata come attributo
+        lettereGriglia[riga][colonna] = new letter(lettera, valore,player,p);
+        mostraGriglia();
+    }
+
+    // Metodo per visualizzare la griglia con le lettere
+    private void mostraGriglia() {
+        for (int row = 0; row < 17; row++) {
+            for (int col = 0; col < 17; col++) {
+                letter letteraCorrente = lettereGriglia[row][col];
+                grigliaDiGioco[row][col].setText(Character.toString(letteraCorrente.getCharacter()));//set della lettera premuta nella cella selezionata grazie a setText
+
+                if (letteraCorrente.getPlayer() != null) {    //controlla se la lettera ha un giocatore associato 
+                    if (turnoGiocatore1 && letteraCorrente.getPlayer().getPlayer_name().equals("Player 1")) {
+                        grigliaDiGioco[row][col].setForeground(Color.RED);//se la lettera appartiene al giocatore 1 la coloro di rosso
+                    } else if (!turnoGiocatore1 && letteraCorrente.getPlayer().getPlayer_name().equals("Player 2")) {
+                        grigliaDiGioco[row][col].setForeground(Color.BLUE); //se la lettera appartiene al giocatore 2 la coloro di blu
+                    }
+                }
+            }
+        }
+        turnoGiocatore1 = !turnoGiocatore1;
+    }
    
 
 }
