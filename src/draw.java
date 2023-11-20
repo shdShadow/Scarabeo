185d948b314815dc997a6c8a1c7a29fc1de53b1d
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-20 12:47:43 +0100

    eliminazione draw di prova

--- src/draw.java
+++ src/draw.java
@@ -17,24 +17,4 @@ public class draw {
         }
     }
 
-    public void mostraMatrice() {
 
-        for (int i = 0; i < matriceDiGioco.length; i++) { // stampo la griglia
-            for (int j = 0; j < matriceDiGioco[i].length; j++) {
-                System.out.print(matriceDiGioco[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    // Aggiorna la posizione nella matrice con un valore specifico
-    public void updateMatrix(int righe, int colonne, char value) {
-        if (righe >= 0 && righe < matriceDiGioco.length && colonne >= 0 && colonne < matriceDiGioco[0].length) { // controllo se la lettera inserita è compresa nella matrice
-                                                                                                                 
-            matriceDiGioco[righe][colonne] = value; // se è compresa
-        } else {
-            System.out.println("Posizione non valida"); // se non è compresa
-        }
-    }
-
-}
