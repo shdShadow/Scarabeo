b4e2b2e34252556635923974c9255c50aa93770a
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-20 12:47:58 +0100

    eliminazione draw.java

--- src/draw.java
+++ src/draw.java
@@ -1,20 +1,2 @@
-public class draw {
-    private char[][] matriceDiGioco;
-
-    public draw(int righe, int colonne) {
-        matriceDiGioco = new char[righe][colonne];
-
-        initializeMatrix();// Inizializzo la matrice con dei valori di esempio
-    }
-
-    private void initializeMatrix() {
-        for (int i = 0; i < matriceDiGioco.length; i++) { // scorro le righe
-            for (int j = 0; j < matriceDiGioco[i].length; j++) { // scorro le colonne
-
-                matriceDiGioco[i][j] = (char) ('A' + (int) (Math.random() * 26)); // genero lettere casuali dalla A alla
-                                                                                  // Z coi valori ASCII
-            }
-        }
-    }
 
 
