b5a4cf625faaac77c3db176e35e51adbd1623326
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-20 16:57:17 +0100

    Disegno scritta 2 sotto scritta1

--- src/server/interfacciaGrafica.java
+++ src/server/interfacciaGrafica.java
@@ -19,15 +19,33 @@ public class interfacciaGrafica {
                 Image image=immagineScarabeo.getImage();
                 g.drawImage(image, 0, 0, getWidth(),getHeight(),this);
 
-                //Disegno scritta SCARABEO AL CENTRO IN ALTO NELLA FINESTRA
+                //Disegno scritta SCARABEO al centro in alto nella finestra
                 String scritta1="SCARABEO";
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Arial",Font.BOLD,30));
-                FontMetrics fontMetricsTitolo=g.getFontMetrics();//prende le info relative al font utilizzato per scritta1
-                int stringWidthTitolo = fontMetricsTitolo.stringWidth(scritta1);//larghezza in pixel 
+                FontMetrics fontMetricsScritta1=g.getFontMetrics();//prende le info relative al font utilizzato per scritta1
+                int stringWidthTitolo = fontMetricsScritta1.stringWidth(scritta1);//larghezza in pixel 
                 int xScritta1 = (getWidth() - stringWidthTitolo) / 2;//per centrare orizzonalmente
                 int yScritta1 = getHeight() / 2 - 120; // Posiziona sopra il messaggio principale
                 g.drawString(scritta1, xScritta1, yScritta1);//disegna scritta1
+
+
+                
+                //Disegno scritta L'unico gioco di parole con lo scarabeo sotto la prima scritta
+                String scritta2 = "L'unico gioco di parole con lo scarabeo";
+                g.setColor(Color.RED);
+                FontMetrics fontMetricsScritta2 = g.getFontMetrics();//prende le info relative al font utilizzato per scritta2
+                int stringWidthScritta2 = fontMetricsScritta2.stringWidth(scritta2);
+                int xScritta2 = (getWidth() - stringWidthScritta2) / 2;
+                int yScritta2 = getHeight() / 2 + 40; // Posiziona sotto il messaggio principale
+                int rectWidth = stringWidthScritta2 + 20; // Aggiungo margine a sinistra e destra
+                int rectHeight = fontMetricsScritta2.getHeight() + 10; // Altezza testo con margine sopra e sotto
+                g.fillRect(xScritta2 - 10, yScritta2 - 30, rectWidth, rectHeight);
+
+                // Disegna la scritta all'interno del rettangolo rosso
+                g.setColor(Color.WHITE);
+                g.drawString(scritta2, xScritta2, yScritta2);
+
 
             }
         }
