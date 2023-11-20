9c1298da9c71c66ae9b29b925cc2f22315b240a5
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-20 16:47:12 +0100

    Disegno scritta SCARABEO centrata in alto nella finestra

--- src/server/interfacciaGrafica.java
+++ src/server/interfacciaGrafica.java
@@ -19,6 +19,16 @@ public class interfacciaGrafica {
                 Image image=immagineScarabeo.getImage();
                 g.drawImage(image, 0, 0, getWidth(),getHeight(),this);
 
+                //Disegno scritta SCARABEO AL CENTRO IN ALTO NELLA FINESTRA
+                String scritta1="SCARABEO";
+                g.setColor(Color.WHITE);
+                g.setFont(new Font("Arial",Font.BOLD,30));
+                FontMetrics fontMetricsTitolo=g.getFontMetrics();//prende le info relative al font utilizzato per scritta1
+                int stringWidthTitolo = fontMetricsTitolo.stringWidth(scritta1);//larghezza in pixel 
+                int xScritta1 = (getWidth() - stringWidthTitolo) / 2;//per centrare orizzonalmente
+                int yScritta1 = getHeight() / 2 - 120; // Posiziona sopra il messaggio principale
+                g.drawString(scritta1, xScritta1, yScritta1);//disegna scritta1
+
             }
         }
     }
