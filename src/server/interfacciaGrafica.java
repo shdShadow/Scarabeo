120b35f474d735c1050446b5216bc487faea7348
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-20 16:37:38 +0100

    Update interfacciaGrafica.java

--- src/server/interfacciaGrafica.java
+++ src/server/interfacciaGrafica.java
@@ -1,5 +1,25 @@
 package server;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class interfacciaGrafica {
-    
+    public interfacciaGrafica() {
+        JFrame frame = new JFrame("Scarabeo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                //posiziono logo dello scarabeo
+                ImageIcon immagineScarabeo=new ImageIcon("scarabeoLogo.png");
+                Image image=immagineScarabeo.getImage();
+                g.drawImage(image, 0, 0, getWidth(),getHeight(),this);
+
+            }
+        }
+    }
 }
