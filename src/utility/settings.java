0f8696d361025a9ca74ace7ce9f7ffe5b7f35fd8
delfo8 <115870814+delfo8@users.noreply.github.com>
2023-11-23 21:02:40 +0100

    documentazione settings

--- src/utility/settings.java
+++ src/utility/settings.java
@@ -1,15 +1,56 @@
 package utility;
 
+/**
+ * Classe contenente costanti utilizzate nel gioco Scarabeo.
+ */
 public class settings {
+    /**
+     * Punteggio massimo per il gioco.
+     */
     public static final Integer max_punteggio = 20;
-    public static final int port = 666;
-    public static final String ip = "127.0.0.1";
-    public static final int loadintWidth = 1280;
-    public static final int loadingHeight = 720;
-    public static final int MATRIX_SIZE = 17;
-    public static final int CELL_SIZE = 35;
-    public static final int GRID_SIZE = MATRIX_SIZE * CELL_SIZE;
-    public static final int WINDOW_WIDTH = 1280;
-    public static final int WINDOW_HEIGHT = 720;
 
+    /**
+     * Numero di porta per la connessione al server.
+     */
+    public static final int port = 666;
+
+    /**
+     * Indirizzo IP del server.
+     */
+    public static final String ip = "127.0.0.1";
+
+    /**
+     * Larghezza della finestra di caricamento.
+     */
+    public static final int loadintWidth = 1280;
+
+    /**
+     * Altezza della finestra di caricamento.
+     */
+    public static final int loadingHeight = 720;
+
+    /**
+     * Dimensione della matrice di gioco.
+     */
+    public static final int MATRIX_SIZE = 17;
+
+    /**
+     * Dimensione di una singola cella nella matrice di gioco.
+     */
+    public static final int CELL_SIZE = 35;
+
+    /**
+     * Dimensione totale della griglia di gioco.
+     */
+    public static final int GRID_SIZE = MATRIX_SIZE * CELL_SIZE;
+
+    /**
+     * Larghezza della finestra di gioco.
+     */
+    public static final int WINDOW_WIDTH = 1280;
+
+    /**
+     * Altezza della finestra di gioco.
+     */
+    public static final int WINDOW_HEIGHT = 720;
 }
