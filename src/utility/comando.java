85efc3912e14029ab04ba7e3d3749e15e60de107
Simone Ballerini <simomosino@gmail.com>
2023-11-23 19:53:52 +0100

    Aggiunta commenti e inizio documentazione con Javadoc

--- src/utility/comando.java
+++ src/utility/comando.java
@@ -2,25 +2,82 @@ package utility;
 
 import java.util.ArrayList;
 
+/**
+ * La classe 'comando' rappresenta un comando inviato dal client al server o
+ * viceversa
+ * Contiene informazioni riguardo al comando da eseguire inerente alla lista di
+ * lettere annessa
+ */
 public class comando {
-  //comando inviato dal client al server
+  // ATTRIBUTI
+
+  /**
+   * Rappresenta il comando che verra' eseguito
+   * 
+   */
   private String exec;
-  //lista di lettere in caso di comando 'add', quindi di parola aggiunta
+  /**
+   * Rappresenta la lista di lettere che verra' utilizzata in base al comando
+   * richiesto
+   * 
+   */
   private ArrayList<letter> l;
 
-  //COSTRUTTORI
+  // COSTRUTTORI
+  /**
+   * Costruttore di default
+   * 
+   */
   public comando() {
     exec = "";
     l = new ArrayList<letter>();
   }
 
+  /**
+   * Costruttore parametrico
+   * 
+   * @param _exec comando da eseguire
+   * @param _l    lista di lettere da utilizzare
+   */
   public comando(String _exec, ArrayList<letter> _l) {
     exec = _exec;
     l = _l;
   }
-  //GETTERS E SETTERS
-  public ArrayList<letter> getL() { return l; }
-  public String getExec() { return exec; }
-  public void setL(ArrayList<letter> l) { this.l = l; }
-  public void setExec(String exec) { this.exec = exec; }
+
+  // GETTER E SETTER
+  /**
+   * Ritorna la lista di lettere
+   * 
+   * @return la lista di lettere
+   */
+  public ArrayList<letter> getL() {
+    return l;
+  }
+
+  /**
+   * Ritorna il comando da eseguire
+   * 
+   * @return il comando da eseguire
+   */
+  public String getExec() {
+    return exec;
+  }
+
+  /**
+   * Imposta la lista di lettere
+   * 
+   * @param l la lista di lettere
+   */
+  public void setL(ArrayList<letter> l) {
+    this.l = l;
+  }
+
+  /**
+   * Imposta il comando da eseguire
+   * 
+   * @param exec il comando da eseguire
+   */
+  public void setExec(String exec) {
+    this.exec = exec;
+  }
 }
--- src/utility/letter.java
+++ src/utility/letter.java
@@ -8,51 +8,113 @@ import java.util.ArrayList;
  * posizione
  */
 public class letter {
+  // ATTRIBUTI
+  /**
+   * Rappresenta il carattere che verra' poi rappresentato in gioco
+   */
   private char character;
+  /**
+   * Rappresenta il punteggio della lettera
+   * Ha valore solo quando viene caricato tramite il file xml "letters_value.xml".
+   * I client non hanno bisogno di sapere il valore della lettera in quanto il
+   * punteggio viene poi calcolato dal server
+   */
   private Integer value;
+  /**
+   * Rappresenta il giocatore che ha utilizzato la lettera
+   */
   private player player;
+  /**
+   * Rappresenta la posizione della lettera all'interno della matrice di gioco
+   */
   private punto p;
+  /**
+   * Rappresenta se la lettera e' stata presa in prestito.
+   * Se e' true, la lettera proviene da un' altra lettera gia' presente sul campo
+   * di gioco
+   */
   public boolean borrowed = false;
 
+  // GETTER E SETTER
   /**
-   * Returns the character value of this Letter.
+   * Ritorna il carattere della lettera
    *
-   * @return the character value of this Letter
+   * @return il carattere dell' oggetto lettera corrente
+   * 
    */
   public char getCharacter() {
     return character;
   }
 
+  /**
+   * Restituisce il giocatore associato a questa lettera.
+   *
+   * @return l'oggetto player associato a questa lettera
+   */
   public player getPlayer() {
     return player;
   }
 
+  /**
+   * Restituisce il punteggio della lettera.
+   *
+   * @return il punteggio della lettera
+   */
   public Integer getValue() {
     return value;
   }
 
+  /**
+   * Restituisce la posizione della lettera nella matrice di gioco.
+   *
+   * @return l'oggetto punto che rappresenta la posizione della lettera nella
+   *         matrice di gioco
+   */
   public punto getP() {
     return p;
   }
 
+  /**
+   * Imposta il punteggio della lettera.
+   *
+   * @param p il punteggio della lettera
+   */
   public void setP(punto p) {
     this.p = p;
   }
 
+  /**
+   * Imposta il carattere che verra' visualizzato della lettera.
+   *
+   * @param character il carattere della lettera
+   */
   public void setCharacter(char character) {
     this.character = character;
   }
 
+  /**
+   * Imposta il player associato alla lettera.
+   *
+   * @param player l'oggetto player che verra' associato alla lettera
+   */
   public void setPlayer(player player) {
     this.player = player;
   }
 
+  /**
+   * Ritorna il valore dell'atributo borrowed.
+   *
+   * @return il valore dell'atributo borrowed
+   */
   public Boolean getBorrowed() {
     return borrowed;
   }
 
-  // CONSTRUCTORS
+  // CONSTRUTTORI
 
+  /**
+   * Costruttore di default.
+   */
   public letter() {
     character = ' ';
     value = 0;
@@ -60,13 +122,13 @@ public class letter {
     p = new punto();
   }
 
-  public letter(char _character, Integer _value, player _player, punto _p) {
-    character = _character;
-    value = _value;
-    player = _player;
-    p = _p;
-  }
-
+  /**
+   * Costruttore parametrico utilizzato dalla classe fileManager per caricare il
+   * valore delle lettere leggendo dal file "letters_value.xml"
+   *
+   * @param _character il carattere della lettera
+   * @param _value     il punteggio della lettera
+   */
   public letter(char _character, Integer _value) {
     character = _character;
     value = _value;
@@ -74,13 +136,17 @@ public class letter {
     p = new punto();
   }
 
-  public letter(char _character, Integer _value, punto _p) {
-    character = _character;
-    value = _value;
-    player = new player();
-    p = _p;
-  }
-
+  /**
+   * Costruttore parametrico utilizzato dalla classe parserStringifier per parsare
+   * un comando ricevuto
+   *
+   * @param _character il carattere della lettera
+   * @param _player    il giocatore che ha utilizzato la lettera
+   * @param _p         la posizione della lettere nella matrice di gioco
+   * @param _value     il punteggio della lettera
+   * @param _borrowed  se la lettera e' stata presa in prestito da un altra parola
+   *                   gia' presente sul campo di gioco
+   */
   public letter(char _character, Integer _value, player _player, punto _p, Boolean _borrowed) {
     character = _character;
     value = _value;
@@ -89,13 +155,14 @@ public class letter {
     p = _p;
   }
 
-  public letter(char _character, punto _p) {
-    character = _character;
-    value = 0;
-    player = new player();
-    p = _p;
-  }
-
+  /**
+   * Costruttore parametrico utilizzato dalla classe drawMatrice in fase di
+   * inserimento di una lettera
+   *
+   * @param _character il carattere della lettera
+   * @param _p         la posizione della lettera nella matrice di gioco
+   * @param _player    il giocatore che ha utilizzato la lettera
+   */
   public letter(char _character, punto _p, player _player) {
     character = _character;
     value = 0;
@@ -104,24 +171,12 @@ public class letter {
   }
 
   /**
-   * Returns a string representation of the `letter` object.
+   * Ritorna una stringa formata dalla concatenazione dei caratteri della lista di
+   * oggetti lettera fornita
    *
-   * @return a string representation of the `letter` object
-   */
-  public String toString() {
-    String s = "";
-    s += "Char: " + character + " Value: " + value +
-        " Player: " + player.getPlayer_name();
-    return s;
-  }
-
-  /**
-   * Returns a word formed by concatenating the characters of the given list of
-   * letters.
-   *
-   * @param l the list of letters
-   * @return a word formed by concatenating the characters of the given list of
-   *         letters
+   * @param l la lista di oggetti lettera
+   * @return una stringa rappresentante una parola generata dalla concatenazione
+   *         dei caratteri della lista di oggetti lettera fornita
    */
   public static String returnWord(ArrayList<letter> l) {
     String s = "";
