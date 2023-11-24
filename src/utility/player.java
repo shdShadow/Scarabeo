cdf353cfd84fe0235679a473abebd1736acab745
shdShadow <simomosino@gmail.com>
2023-11-24 10:00:56 +0100

    Aggiornata documentazione

--- src/utility/player.java
+++ src/utility/player.java
@@ -1,41 +1,60 @@
 package utility;
-
+/**
+ * Classe che rappresenta un giocatore. In essa sono contenuti il nome del giocatore e il suo punteggio
+ */
 public class player {
+    //ATTRIBUTI
     
+    /**
+     * Rappresenta il nome di un giocatore
+     */
     private String player_name;
+    /**
+     * Rappresenta il punteggio totale di un giocatore
+     */
     private Integer total_points;
 
-    // GETTERS AND SETTERS
+    // GETTER AND SETTER
 
+
+    /**
+     * Ritorna il punteggio totale di un giocatore
+     *
+     * @return il punteggio totale del giocatore
+     */
     public Integer getTotal_points() {
         return total_points;
     }
-
+  
+    /**
+     * Ritorna il nome del giocatore
+     *
+     * @return il nome del giocatore
+     */
     public String getPlayer_name() {
         return player_name;
     }
 
 
-    // CONTSTRUCTORS
+    // COSTRUTTORI
 
+    /**
+     * Costruttore di default
+     */
     public player() {
         player_name = "";
         total_points = 0;
 
     }
-
-    public player(String _player_name, Integer _total_points, letter[] _current_letters) {
-        this.player_name = _player_name;
-        this.total_points = _total_points;
-
-    }
-
+    /**
+     * Costruttore parametrico
+     *
+     * @param _player_name nome del giocatore
+     */
     public player(String _player_name) {
         this.player_name = _player_name;
         this.total_points = 0;
 
     }
-
-    // METHODS
 
 }
--- src/utility/punto.java
+++ src/utility/punto.java
@@ -1,28 +1,76 @@
 package utility;
+/**
+ * Classe che rappresenta un punto. Sono contenute le coordinate x e y di un punto all'interno della matrice di gioco
+ */
 public class punto {
   // ATTRIBUTI
 
-    /**
-     * attributo contenente le coordinate x del punto
-     */
+  /**
+   * attributo contenente le coordinate x del punto
+   */
   private Integer x;
   /**
    * attributo contenente le coordinate y del punto
-  */
+   */
   private Integer y;
-  //COSTRUTTORI
+
+  // COSTRUTTORI
+
+  /**
+   * Costruttore di default
+   *
+   */
   public punto() {
     x = 0;
     y = 0;
   }
 
+  /**
+   * Costruttore parametrico
+   *
+   * @param _x coordinata x del punto
+   * @param _y coordinata y del punto
+   */
   public punto(Integer _x, Integer _y) {
     x = _x;
     y = _y;
   }
-  //GETTERS E SETTERS
-  public Integer getX() { return x; }
-  public Integer getY() { return y; }
-  public void setX(Integer x) { this.x = x; }
-  public void setY(Integer y) { this.y = y; }
+
+  // GETTER E SETTER
+
+  /**
+   * Ritorna la coordinata x del punto
+   *
+   * @return la coordinata x del punto
+   */
+  public Integer getX() {
+    return x;
+  }
+
+  /**
+   * Ritorna la coordinata y del punto
+   *
+   * @return la coordinata y del punto
+   */
+  public Integer getY() {
+    return y;
+  }
+
+  /**
+   * Setta la coordinata x del punto
+   *
+   * @param x coordinata x del punto
+   */
+  public void setX(Integer x) {
+    this.x = x;
+  }
+
+  /**
+   * Setta la coordinata y del punto
+   *
+   * @param y coordinata y del punto
+   */
+  public void setY(Integer y) {
+    this.y = y;
+  }
 }
